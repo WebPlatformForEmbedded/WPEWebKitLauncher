9f7b3e135f305ae32cd7c343ad3b008d9a2bbbde
Sander van der Maar <s.van.der.maar@metrological.com>
2015-10-23 15:58:50 +0200

    Makes wpe-launcher work with Intel CE WPE WebKit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <WPE/WebKit.h>
+
+#if 0
 #include <bcm_host.h>
+#endif
+
+#include <libgdl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
 #include <glib.h>
 
 WKPageNavigationClientV0 s_navigationClient = {
@@ -33,9 +42,86 @@ WKPageNavigationClientV0 s_navigationClient = {
     nullptr, // didRemoveNavigationGestureSnapshot
 };
 
+
+#if 0
+
+// Plane size and position
+#define ORIGIN_X 0
+#define ORIGIN_Y 0
+#define WIDTH 1280
+#define HEIGHT 720
+#define ASPECT ((GLfloat)WIDTH / (GLfloat)HEIGHT)
+
+// Initializes a plane for the graphics to be rendered to
+static gdl_ret_t setup_plane(gdl_plane_id_t plane) {
+    gdl_pixel_format_t pixelFormat = GDL_PF_ARGB_32;
+    gdl_color_space_t colorSpace = GDL_COLOR_SPACE_RGB;
+    gdl_rectangle_t srcRect;
+    gdl_rectangle_t dstRect;
+    gdl_ret_t rc = GDL_SUCCESS;
+
+    dstRect.origin.x = ORIGIN_X;
+    dstRect.origin.y = ORIGIN_Y;
+    dstRect.width = WIDTH;
+    dstRect.height = HEIGHT;
+
+    srcRect.origin.x = 0;
+    srcRect.origin.y = 0;
+    srcRect.width = WIDTH;
+    srcRect.height = HEIGHT;
+
+    rc = gdl_plane_reset(plane);
+    if (GDL_SUCCESS == rc) {
+        rc = gdl_plane_config_begin(plane);
+    }
+
+    if (GDL_SUCCESS == rc) {
+        rc = gdl_plane_set_attr(GDL_PLANE_SRC_COLOR_SPACE, &colorSpace);
+    }
+
+    if (GDL_SUCCESS == rc) {
+        rc = gdl_plane_set_attr(GDL_PLANE_PIXEL_FORMAT, &pixelFormat);
+    }
+
+    if (GDL_SUCCESS == rc) {
+        rc = gdl_plane_set_attr(GDL_PLANE_DST_RECT, &dstRect);
+    }
+
+    if (GDL_SUCCESS == rc) {
+        rc = gdl_plane_set_attr(GDL_PLANE_SRC_RECT, &srcRect);
+    }
+
+    if (GDL_SUCCESS == rc) {
+        rc = gdl_plane_config_end(GDL_FALSE);
+    } else {
+        gdl_plane_config_end(GDL_TRUE);
+    }
+
+    if (GDL_SUCCESS != rc) {
+        fprintf(stderr, "GDL configuration failed! GDL error code is 0x%x\n",
+                rc);
+    }
+
+    return rc;
+}
+
+#endif
+
 int main(int argc, char* argv[])
 {
+    #if 0
     bcm_host_init();
+    #endif
+
+    #if 0
+    printf("[%d]+gdl-init\n", getpid());
+
+    gdl_plane_id_t plane = GDL_PLANE_ID_UPP_C;
+    gdl_init(0);
+    setup_plane(plane);
+
+    printf("[%d]+gdl-init\n", getpid());
+    #endif
 
     GMainLoop* loop = g_main_loop_new(nullptr, FALSE);
 
@@ -45,20 +131,19 @@ int main(int argc, char* argv[])
     auto pageGroup = WKPageGroupCreateWithIdentifier(pageGroupIdentifier);
     WKRelease(pageGroupIdentifier);
 
-    auto preferences = WKPreferencesCreate();
-    // Allow mixed content.
-    WKPreferencesSetAllowRunningOfInsecureContent(preferences, true);
-    WKPreferencesSetAllowDisplayOfInsecureContent(preferences, true);
-    WKPageGroupSetPreferences(pageGroup, preferences);
-
     auto pageConfiguration  = WKPageConfigurationCreate();
     WKPageConfigurationSetContext(pageConfiguration, context);
     WKPageConfigurationSetPageGroup(pageConfiguration, pageGroup);
 
     auto view = WKViewCreate(pageConfiguration);
 
+    #if 0
     uint32_t width = 0, height = 0;
     graphics_get_display_size(DISPMANX_ID_HDMI, &width, &height);
+    #endif
+    uint32_t width = 1280, height = 720;
+
+
     WKViewResize(view, WKSizeMake(width, height));
 
     auto page = WKViewGetPage(view);
@@ -78,7 +163,6 @@ int main(int argc, char* argv[])
     WKRelease(pageConfiguration);
     WKRelease(pageGroup);
     WKRelease(context);
-    WKRelease(preferences);
     g_main_loop_unref(loop);
     return 0;
 }
