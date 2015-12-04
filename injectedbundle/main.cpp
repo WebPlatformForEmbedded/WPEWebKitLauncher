ac1b25e42621b4a656fd1a543480afc8fa9135a3
Zan Dobersek <zdobersek@igalia.com>
2015-12-04 04:53:38 -0800

    Injected bundle: add a really basic example of a JS extension.

--- injectedbundle/main.cpp
+++ injectedbundle/main.cpp
@@ -12,9 +12,135 @@ std::string toSTDString(WKStringRef string)
     return std::string(buffer.get(), stringLength - 1);
 }
 
+static void injectJSExtensions(JSGlobalContextRef context)
+{
+    static JSValueRef s_value1 = JSValueMakeNumber(context, 15);
+    static JSValueRef s_value2 = JSValueMakeNumber(context, 16);
+
+    static JSStaticValue s_staticValues[] = {
+        {
+            "value1", // name
+            // getProperty
+            [](JSContextRef context, JSObjectRef, JSStringRef, JSValueRef*) -> JSValueRef {
+                fprintf(stderr, "[WPE-EXT] getProperty for 'value1' invoked.\n");
+                return s_value1;
+            },
+            nullptr, // setProperty
+            kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete, // attributes
+        },
+        {
+            "value2", // name
+            // getProperty
+            [](JSContextRef context, JSObjectRef, JSStringRef, JSValueRef*) -> JSValueRef {
+                fprintf(stderr, "[WPE-EXT] getProperty for 'value2' invoked.\n");
+                return s_value2;
+            },
+            // setProperty
+            [](JSContextRef, JSObjectRef, JSStringRef, JSValueRef value, JSValueRef*) -> bool {
+                fprintf(stderr, "[WPE-EXT] setProperty for 'value2' invoked.\n");
+                s_value2 = value;
+                return true;
+            },
+            kJSPropertyAttributeNone, // attributes
+        },
+        { nullptr, nullptr, nullptr, 0 }
+    };
+
+    static JSStaticFunction s_staticFunctions[] = {
+        {
+            "function1", // name
+            // callAsFunction
+            [](JSContextRef context, JSObjectRef, JSObjectRef, size_t argumentCount, const JSValueRef arguments[], JSValueRef*) -> JSValueRef {
+                fprintf(stderr, "[WPE-EXT] callAsFunction for 'function1' invoked.\n");
+
+                JSStringRef returnString = JSStringCreateWithUTF8CString("function1 return value");
+                JSValueRef returnValue = JSValueMakeString(context, returnString);
+                JSStringRelease(returnString);
+                return returnValue;
+            },
+            kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete, // attributes
+        },
+        { nullptr, nullptr, 0 }
+    };
+    static JSClassDefinition s_classDefinition = {
+        0, // version
+        kJSClassAttributeNone, //attributes
+        "WPE", // className
+        0, // parentClass
+        s_staticValues, // staticValues
+        s_staticFunctions, // staticFunctions
+        nullptr, //initialize
+        nullptr, //finalize
+        nullptr, //hasProperty
+        nullptr, //getProperty
+        nullptr, //setProperty
+        nullptr, //deleteProperty
+        nullptr, //getPropertyNames
+        nullptr, //callAsFunction
+        nullptr, //callAsConstructor
+        nullptr, //hasInstance
+        nullptr, //convertToType
+    };
+
+    JSClassRef wpeClass = JSClassCreate(&s_classDefinition);
+    JSValueRef wpeObject = JSObjectMake(context, wpeClass, nullptr);
+    JSClassRelease(wpeClass);
+
+    JSStringRef extensionString = JSStringCreateWithUTF8CString("wpe");
+    JSObjectSetProperty(context, JSContextGetGlobalObject(context), extensionString, wpeObject,
+        kJSPropertyAttributeReadOnly | kJSPropertyAttributeDontDelete, nullptr);
+    JSStringRelease(extensionString);
+}
+
+static WKBundlePageLoaderClientV6 s_pageLoaderClient = {
+    { 6, nullptr },
+    nullptr, // didStartProvisionalLoadForFrame
+    nullptr, // didReceiveServerRedirectForProvisionalLoadForFrame
+    nullptr, // didFailProvisionalLoadWithErrorForFrame
+    nullptr, // didCommitLoadForFrame
+    nullptr, // didFinishDocumentLoadForFrame
+    nullptr, // didFinishLoadForFrame
+    nullptr, // didFailLoadWithErrorForFrame
+    nullptr, // didSameDocumentNavigationForFrame
+    nullptr, // didReceiveTitleForFrame
+    nullptr, // didFirstLayoutForFrame
+    nullptr, // didFirstVisuallyNonEmptyLayoutForFrame
+    nullptr, // didRemoveFrameFromHierarchy
+    nullptr, // didDisplayInsecureContentForFrame
+    nullptr, // didRunInsecureContentForFrame
+    // didClearWindowObjectForFrame
+    [](WKBundlePageRef, WKBundleFrameRef frame, WKBundleScriptWorldRef scriptWorld, const void*) {
+        fprintf(stderr, "didClearWindowObjectForFrame\n");
+        JSGlobalContextRef context = WKBundleFrameGetJavaScriptContextForWorld(frame, scriptWorld);
+        injectJSExtensions(context);
+    },
+    nullptr, // didCancelClientRedirectForFrame
+    nullptr, // willPerformClientRedirectForFrame
+    nullptr, // didHandleOnloadEventsForFrame
+    nullptr, // didLayoutForFrame
+    nullptr, // didNewFirstVisuallyNonEmptyLayout_unavailable
+    nullptr, // didDetectXSSForFrame
+    nullptr, // shouldGoToBackForwardListItem
+    nullptr, // globalObjectIsAvailableForFrame
+    nullptr, // willDisconnectDOMWindowExtensionFromGlobalObject
+    nullptr, // didReconnectDOMWindowExtensionToGlobalObject
+    nullptr, // willDestroyGlobalObjectForDOMWindowExtension
+    nullptr, // didFinishProgress
+    nullptr, // shouldForceUniversalAccessFromLocalURL
+    nullptr, // didReceiveIntentForFrame_unavailable
+    nullptr, // registerIntentServiceForFrame_unavailable
+    nullptr, // didLayout
+    nullptr, // featuresUsedInPage
+    nullptr, // willLoadURLRequest
+    nullptr, // willLoadDataRequest
+};
+
 static WKBundleClientV1 s_bundleClient = {
     { 1, nullptr },
-    nullptr, // didCreatePage
+    // didCreatePage
+    [](WKBundleRef, WKBundlePageRef page, const void*) {
+        WKBundlePageSetPageLoaderClient(page, &s_pageLoaderClient.base);
+    },
     nullptr, // willDestroyPage
     nullptr, // didInitializePageGroup
     nullptr, // didReceiveMessage
@@ -33,13 +159,10 @@ static WKBundleClientV1 s_bundleClient = {
     },
 };
 
-extern "C" {
-
+extern "C"
 void WKBundleInitialize(WKBundleRef bundle, WKTypeRef)
 {
     fprintf(stderr, "[WPEInjectedBundle] Initialized.\n");
 
     WKBundleSetClient(bundle, &s_bundleClient.base);
-}
-
 }
